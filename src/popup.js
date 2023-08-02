11acca137b5cc87482ef49dde8a9d7fdbbd33b2a
Hangyeol-SSeo <dongha313@gmail.com>
2023-08-02 12:37:21 +0900

    style: popup 스타일 수정

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,23 @@
-// popup.js
-
-// 데이터를 화면에 표시하는 함수
 function displayVideoSources(videoSources) {
     const listElement = document.getElementById('videoList');
-    videoSources.forEach(videoSource => {
-        const listItem = document.createElement('li');
-        listItem.textContent = videoSource.subject;
-        listElement.appendChild(listItem);
-    });
+    const noVideoMessage = document.getElementById('noVideoMessage');
+
+    if (videoSources.length === 0) {
+        noVideoMessage.style.display = 'block';
+    } else {
+        noVideoMessage.style.display = 'none';
+        videoSources.forEach(videoSource => {
+            const listItem = document.createElement('li');
+            listItem.textContent = videoSource.subject;
+
+            // 다운로드 버튼 추가
+            const downloadBtn = document.createElement('button');
+            downloadBtn.className = 'download-btn';
+            listItem.appendChild(downloadBtn);
+
+            listElement.appendChild(listItem);
+        });
+    }
 }
 
 // 팝업이 로드될 때 저장된 정보를 화면에 표시
